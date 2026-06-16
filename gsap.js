f125de923bccc8f2ece12641569d18786da981e4
RoyBrummelink <410010972@st.roc.a12.nl>
2026-06-16 12:08:12 +0200

    v

--- gsap.js
+++ gsap.js
@@ -1,7 +1,11 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 let smoother = ScrollSmoother.create({
-    smooth: 2,
+    smooth: 1,
     effects: true,
     normalizeScroll: true
 });
+
+function scrollCaseStudies(){
+        smoother.scrollTo(".case-studies", true);
+}
